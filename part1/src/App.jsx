efd8959abbac14badd3cf36bf59d930bda2042ee
Puppe <parkkinenjesper@gmail.com>
2024-08-01 16:59:33 +0300

    part1b

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+const Header = (props) =>{
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+const Content = (props) =>{
+  const [part1, part2, part3] = props.parts
+  console.log(props)
+  
+  const Part = (props) =>{
+  console.log(props)
+    return (
+    <>
+      <p>{props.part.name}. Exercises: {props.part.exercises}</p>
+    </>
+    )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Part part = {part1}/>
+      <Part part = {part2}/>
+      <Part part = {part3}/>
+    </div>
+  )
+}
+const Total = (props) => {
+  console.log(props)
+  const totalExcercises = parts => {
+    console.log(parts)
+    return parts[0].exercises + parts[1].exercises + parts[2].exercises
+  }
+  return (
+  <div>
+    <p>Number of exercises: {totalExcercises(props.parts)} </p>
+  </div>
+  )
+}
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>  
+    </div>
   )
 }
 
