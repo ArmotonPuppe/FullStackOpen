ec26d236edcf37e394b925c0c8d6fb76cc63aa7e
Puppe <parkkinenjesper@gmail.com>
2024-08-13 20:16:19 +0300

    2.5 exercises done

--- part2/Course information/src/App.jsx
+++ part2/Course information/src/App.jsx
@@ -1,46 +1,5 @@
+import Course from './components/Course'
 
-const Part = ({part}) =>{
-  const {name, exercises} = part
-  console.log(name, exercises)
-    return (
-    <>
-      {name}. Exercises: {exercises}
-    </>
-    )
-}
-const Content = ({parts}) =>{
-  console.log(parts)
-  
-  
-
-  return (
-    <ul>
-      {parts.map(part => 
-        <li key={part.id}>
-          <Part part={part}/>
-        </li>)} 
-    </ul>
-  )
-}
-const Total = ({parts}) => {
-  console.log(parts)
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return (
-    <b>Total of {total} exercises </b>
-  )
-}
-const Course = (props) =>{
-  console.log(props)
-  const Header = ({name}) => <h2>{name}</h2>
-
-  return(
-    <div>
-      <Header name={props.course.name}/>
-      <Content parts={props.course.parts}/>
-      <Total parts={props.course.parts}/>
-    </div>
-  )
-}
 const App = () => {
   const courses = [
     {
