e8025940b11d436881c5dd2d4a49a5907ddc2dbe
Puppe <parkkinenjesper@gmail.com>
2024-11-04 17:46:35 +0200

    please work

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -15,18 +15,34 @@ const RenderCountries = ({countries, showCountry}) => {
     </div>
   )
 }
-const RenderSingleCountry = ({name, population, area, flag}) => {
-  console.log(flag.toLowerCase())
+
+const RenderSingleCountry = ({name, capital, population, area, flag, temp, weatherIcon, wind}) => {
+  
   return(
     <div>
-      Name: {name}
-      Population: {population}
-      Area: {area}
-      Flag: {<img
-              src={flag}
-              alt={`Flag of ${name}`}
-              width="200"
-              height="100"/>}
+      <p>Name: {name}</p>
+      <p>Capital: {capital}</p>
+      <p>Population: {population}</p>
+      <p>Area: {area}</p>
+      {<img
+        src={flag}
+        alt={`Flag of ${name}`}
+        width="200"
+        height="100"/>}
+      <RenderWeather capital = {capital} temp = {temp} icon={weatherIcon} wind={wind}/>
+    </div>
+  )
+}
+const RenderWeather = ({capital, temp, icon, wind}) =>{
+  return (
+    <div>
+      <p>Weather in {capital}</p>
+      <p>Temperature: {temp} celsius </p>
+      {<img
+        src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+        width="100"
+        height="100"/>}
+      <p>Wind {wind} m/s</p>
     </div>
   )
 }
@@ -35,7 +51,10 @@ const App = () => {
   const [search, setSearch] = useState([])
   const [country, setCountry] = useState(null)
   const [countries, setCountries] = useState([])
-
+  const [weather, setWeather] = useState({})
+  
+  const api_key = import.meta.env.VITE_WEATHER_KEY
+  const testiTuuli = '100'
   useEffect(() => {
       axios
         .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
@@ -52,7 +71,7 @@ const App = () => {
         c => c.name.common.toLowerCase().includes(value.toLowerCase()
         ))
       countrySearch.length === 1
-        ? setCountry(countrySearch[0])
+        ? (setCountry(countrySearch[0]), getWeather(countrySearch[0].capital))
         : (setSearch(countrySearch), setCountry(null))
     }else{
       setSearch([])
@@ -61,20 +80,43 @@ const App = () => {
 
   }, [value, countries])
 
+  const getWeather = (capital) =>{
+    axios
+      .get(`http://api.openweathermap.org/geo/1.0/direct?q=${capital}&limit=1&appid=${api_key}`)
+      .then(response => {
+        const {lat, lon} = response.data[0]
+        axios
+          .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`)
+          .then(response => {
+            setWeather(response.data)
+            console.log(response.data)
+          })
+      })
+  }
+
   
   const showCountry = (country) =>{
     setCountry(country)
+    setWeather(getWeather(country.capital))
   }
   const handleChange = (event) =>{
     setValue(event.target.value)
     console.log('value is', event.target.value)
   }
-
+  
   return (
     <div>
+      {console.log(weather)}
       Country: <input value={value} onChange={handleChange}/>
-      {country
-        ? <RenderSingleCountry name={country.name.common} population={country.population} area={country.area} flag={country.flags.png}/>
+      {country && weather
+        ? <RenderSingleCountry name={country.name.common} 
+                               capital={country.capital} 
+                               population={country.population} 
+                               area={country.area} 
+                               flag={country.flags.png} 
+                               temp = {testiTuuli} 
+                               weatherIcon = {testiTuuli}
+                               wind = {testiTuuli}/>
         : <RenderCountries countries={search} showCountry = {showCountry}/>}
       
     </div>
